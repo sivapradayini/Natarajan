2906460d47d29e1351971761de40e027e8b283b4
Sivapradayini Neelappan <63453793+sivapradayini@users.noreply.github.com>
2020-08-25 17:39:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
-
- function show(){
-                document.getElementById("data").style.display = "block";
-                document.getElementById('poster').style.display = "block";
-                
-                
-                var hlad= document.createElement('div');
-                hlad.style.paddingLeft = "300px"
-                hlad.style.width = "900px";
-                hlad.style.height = "430px";
-                hlad.style.margin = "10px";
-                hlad.style.background = "URL(hladvt.png)";
-                document.body.appendChild(hlad);
-            }
+function show() {
+    document.getElementById("data").style.display = "block";
+    document.getElementById('poster').style.display = "block";
+    var hlad = document.createElement('div');
+    hlad.style.paddingLeft = "300px"
+    hlad.style.width = "900px";
+    hlad.style.height = "430px";
+    hlad.style.margin = "10px";
+    hlad.style.background = "URL(hladvt.png)";
+    document.body.appendChild(hlad);
+}
